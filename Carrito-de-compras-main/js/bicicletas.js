5fae96690d84c51c473a13cd1b4183d3c846d79a
johan_vitonas <88344593+JohanVitonas@users.noreply.github.com>
2024-10-25 20:46:55 -0500

    first commit

---
+++ Carrito-de-compras-main/js/bicicletas.js
@@ -0,0 +1,90 @@
+/* Esta lista podría ser reemplazada por la respuesta de un backend */
+const bicicletas = [
+  {
+    id:1,
+    nombre: "Veloziraptor",
+    precio: 100,
+  },
+  {
+    id:2,
+    nombre: "Biciclón",
+    precio: 200,
+  },
+  {
+    id:3,
+    nombre: "CicloTravesura",
+    precio: 300,
+  },
+  {
+    id:4,
+    nombre: "Pedalástica",
+    precio: 400,
+  },
+  {
+    id:5,
+    nombre: "RuedaFrenesí",
+    precio: 500,
+  },
+  {
+    id:6,
+    nombre: "CicloLoco",
+    precio: 600,
+  },
+
+  {
+    id:7,
+    nombre: "CicloPRO",
+    precio: 700,
+  },
+
+  {
+    id:8,
+    nombre: "CicloPrime",
+    precio: 750,
+  },
+
+  {
+    id:9,
+    nombre: "CicloLoco",
+    precio: 600,
+  },
+
+  {
+    id:10,
+    nombre: "CicloPRO",
+    precio: 700,
+  },  
+  
+  {
+    id:11,
+    nombre: "CicloLoco",
+    precio: 600,
+  },
+
+  {
+    id:12,
+    nombre: "CicloPRO",
+    precio: 700,
+  },  
+
+  {
+    id:14,
+    nombre: "CicloPRO",
+    precio: 700,
+  },   
+
+  {
+    id:18,
+    nombre: "CicloPRO",
+    precio: 700,
+  },
+  
+  {
+    id:19,
+    nombre: "CicloPRO",
+    precio: 700,
+  },
+
+
+
+]
